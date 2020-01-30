1fdecfe734493f33618c820f4dd5afbcd833dc6c
jlb1999 <jlbroughton88@gmail.com>
2020-01-30 07:37:32 -0500

    removed build from .gitignore

---
+++ client/build/precache-manifest.59fe2e21a0bc7727b687532175048e9a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988c1a201001fae2a93d86ae366b5c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa6c02cbfe29b6f83f79",
+    "url": "/static/css/main.9f73592c.chunk.css"
+  },
+  {
+    "revision": "308a59ac908cee6cd752",
+    "url": "/static/js/2.115a8d8b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.115a8d8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa6c02cbfe29b6f83f79",
+    "url": "/static/js/main.7cd36e4c.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "9da49bca426443966b4b9e7d8c2f2b66",
+    "url": "/static/media/chevytruck1.9da49bca.JPG"
+  },
+  {
+    "revision": "548648ba3ce9da243e5d9e2f61158029",
+    "url": "/static/media/chevyvan1.548648ba.jpeg"
+  },
+  {
+    "revision": "cbf26ec3a6fff5b233a80e663081b738",
+    "url": "/static/media/genesis1.cbf26ec3.JPG"
+  },
+  {
+    "revision": "85613ed3b8a38d6ade5825bef4cab0e8",
+    "url": "/static/media/genesis2.85613ed3.JPG"
+  },
+  {
+    "revision": "eefa2e49f3a7d59ffb932f337cbe6157",
+    "url": "/static/media/genesis3.eefa2e49.JPG"
+  },
+  {
+    "revision": "14a2e731a4293ce1c39493985e10bf92",
+    "url": "/static/media/pilot1.14a2e731.JPG"
+  },
+  {
+    "revision": "bbd93575295ba487bb37331780cc644c",
+    "url": "/static/media/smart1.bbd93575.JPG"
+  },
+  {
+    "revision": "0f628cb19c30a2b760b4d06d38a3aaab",
+    "url": "/static/media/smart2.0f628cb1.JPG"
+  }
+]);
